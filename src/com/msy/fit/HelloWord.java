78e42f55b864cf0bdaf5f4dd32811c24838e6446
monster <monster8alien@vip.qq.com>
2020-07-16 17:44:26 +0800

    tyj删除了一行代码

--- src/com/msy/fit/HelloWord.java
+++ src/com/msy/fit/HelloWord.java
@@ -4,6 +4,6 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("hell word");
 		System.out.println("这是谭亚军修改的代码");
-		System.out.println("这是毛世玉修改的代码");
+		// tyj 将msy新增的代码删除掉了
 	}
 }
